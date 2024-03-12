0ca06182f19f8264fab67e3d22d25a2990d76c8f
hyunjinsik <wjd5985@yonsei.ac.kr>
2024-03-13 00:42:42 +0900

    new models added

--- models/resnet.py
+++ models/resnet.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -22,10 +23,10 @@ class BasicBlock(nn.Module):
     '''
     expansion = 1
     
-    def __init__(self, in_planes, planes, stride=1, downsample=None, is_last=False):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, is_last=False):
         super(BasicBlock, self).__init__()
         self.is_last = is_last
-        self.conv1 = conv3x3(in_planes, planes, stride)
+        self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True) #inplace=True로 하면 들어가는 인수 값이 output과 동일하게 변동, 메모리 절약 효과
         self.conv2 = conv3x3(planes, planes)
@@ -35,10 +36,11 @@ class BasicBlock(nn.Module):
     
     def forward(self, x):
         residual = x
-        out = self.conv1(x)
-        out = self.bn1(out) 
-        out = self.relu(out)
         
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
         out = self.conv2(out)
         out = self.bn2(out)
         
@@ -48,14 +50,14 @@ class BasicBlock(nn.Module):
         
         out += residual
         preact = out
-        out = F.relu(x) #init에 쓰는 nn.ReLU와 다르게 forward에서는 F.relu 쓰인다고함
+        out = F.relu(out) #init에 쓰는 nn.ReLU와 다르게 forward에서는 F.relu 쓰인다고함
         if self.is_last:
             return out, preact
         else:
             return out
 
 class ResNet(nn.Module):
-    def __init__(self, depth, num_filters, block_name="BasicBlock", num_classes=100):
+    def __init__(self, depth, num_filters, block_name="BasicBlock", num_classes=10):
         super(ResNet, self).__init__()
     
         if block_name.lower() == "basicblock":
@@ -71,10 +73,22 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, num_filters[0], kernel_size=3, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(num_filters[0])
         self.relu = nn.ReLU(inplace=True)
-        
+        self.layer1 = self._make_layer(block, num_filters[1], n)
+        self.layer2 = self._make_layer(block, num_filters[2], n, stride=2)
+        self.layer3 = self._make_layer(block, num_filters[3], n, stride=2)
+        self.avgpool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(num_filters[3] * block.expansion, num_classes)
+        self.stage_channels = num_filters
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+    
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
-        # 
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
                 nn.Conv2d(
@@ -86,4 +100,79 @@ class ResNet(nn.Module):
                 ),
                 nn.BatchNorm2d(planes * block.expansion),
             )
+        layers = list([])
+        layers.append(
+            block(self.inplanes, planes, stride, downsample, is_last=(blocks == 1))
+        )
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, is_last=(i == blocks - 1)))
         
+        return nn.Sequential(*layers)
+    
+    def get_feat_modules(self):
+        feat_m = nn.ModuleList([])
+        feat_m.append(self.conv1)
+        feat_m.append(self.bn1)
+        feat_m.append(self.relu)
+        feat_m.append(self.layer1)
+        feat_m.append(self.layer2)
+        feat_m.append(self.layer3)
+        return feat_m
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        f0 = x
+
+        x, f1_pre = self.layer1(x)
+        f1 = x
+        x, f2_pre = self.layer2(x)
+        f2 = x
+        x, f3_pre = self.layer3(x)
+        f3 = x
+
+        x = self.avgpool(x)
+        avg = x.reshape(x.size(0), -1)
+        out = self.fc(avg)
+
+        feats = {}
+        feats["feats"] = [f0, f1, f2, f3]
+        feats["preact_feats"] = [f0, f1_pre, f2_pre, f3_pre]
+        feats["pooled_feat"] = avg
+
+        return out
+    
+def resnet8(**kwargs):
+    return ResNet(8, [16, 16, 32, 64], "basicblock", **kwargs)
+
+def resnet20(**kwargs):
+    return ResNet(20, [16, 16, 32, 64], "basicblock", **kwargs)
+
+def resnet32(**kwargs):
+    return ResNet(32, [16, 16, 32, 64], "basicblock", **kwargs)
+
+def resnet44(**kwargs):
+    return ResNet(44, [16, 16, 32, 64], "basicblock", **kwargs)
+
+def resnet56(**kwargs):
+    return ResNet(56, [16, 16, 32, 64], "basicblock", **kwargs)
+
+def resnet110(**kwargs):
+    return ResNet(110, [16, 16, 32, 64], "basicblock", **kwargs)
+
+def resnet8x4(**kwargs):
+    return ResNet(8, [32, 64, 128, 256], "basicblock", **kwargs)
+
+def resnet32x4(**kwargs):
+    return ResNet(32, [32, 64, 128, 256], "basicblock", **kwargs)
+
+if __name__=="__main__":
+    x = torch.randn(2, 3, 32, 32)
+    net = resnet8(num_classes=10)
+    logits = net(x)
+
+    print(logits.shape)
+
+    
