e77a585d15bca13a80e72c80d52f6cc2b0f9dd80
hyunjin <wjd5985@yonsei.ac.kr>
2024-03-14 22:50:38 +0900

    asd

--- utils/train.py
+++ utils/train.py
@@ -152,5 +152,5 @@ if __name__ == "__main__":
     else:
         from ..models import wrn    
     
-    model, model_name = wrn.wrn_16_2(num_classes=100)
+    model, model_name = wrn.wrn_40_2(num_classes=100)
     main(model, model_name)
